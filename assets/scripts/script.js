5e8601c7f030e991d9815a4e7ae95e03c3135706
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-01-27 21:19:33 -0700

    Logic added change colors in js file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,75 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var today = dayjs();
+console.log(today);
+
+$("#currentDay").text(today.format('dddd, MMMM DD'));
+
+// Validate times
+function validateRowTime(){
+    var currentHour = today.format('H');
+    var currentAMPM = today.format('A');
+    console.log(currentHour);
+    console.log(currentAMPM);
+
+    var hourRowEl = $('.hourRow');
+    console.log(hourRowEl);
+    
+
+    hourRowEl.each(function() {
+        var fullText = $(this).text();
+        console.log(fullText);
+        var textWithoutLasTwo = fullText.slice(0,-2);
+        console.log(textWithoutLasTwo);
+        console.log(typeof(textWithoutLasTwo));
+        var lastTwoCha = fullText.slice(-2);
+        console.log(lastTwoCha); //AM or PM
+
+
+        var row = $(this).closest('.row'); // Find the closest parent with class 'row'
+
+        // Remove all classes first
+        row.removeClass('past future present');
+        
+        
+    
+        var rowHour24 = parseInt(textWithoutLasTwo);
+
+        // Convertir horas PM a formato de 24 horas sumando 12
+        if (lastTwoCha === 'PM' && rowHour24 !== 12) {
+            rowHour24 += 12;
+        }
+        /* if(parseInt(textWithoutLasTwo) < parseInt(currentHour) && currentAMPM == lastTwoCha) {
+            row.addClass('past');
+        } else if(parseInt(textWithoutLasTwo) > parseInt(currentHour) && currentAMPM == lastTwoCha) {
+            row.addClass('future');
+        } else {
+            row.addClass('present');
+        } */
+        if ((rowHour24 < parseInt(currentHour))) {
+            row.addClass('past');
+        } else if(rowHour24 > parseInt(currentHour)){
+            row.addClass('future'); 
+        } else if (rowHour24 === parseInt(currentHour) && currentAMPM === lastTwoCha) {
+            row.addClass('present');
+        }
+        
+    })
+}
+
+validateRowTime();
+
+// Create the event for the button
+$('.saveBtn').on('click', function() {
+    var id = $(this).parent().attr('id');
+    var text = $(this).siblings('textarea').val();
+    localStorage.setItem(id, text);
+});
+ // Reload and still the information is saved.
+var events = JSON.parse(localStorage.getItem(id, text));
+
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
